985648f8cf4aab1d471238dcc2034e8d8a83692e
Cristian <cristian.paredes01@epn.edu.ec>
2023-06-06 07:31:28 -0500

    generacion de getters y setter

--- src/figuras2D.java
+++ src/figuras2D.java
@@ -1,2 +1,20 @@
 public class figuras2D {
+   int nLados;// numero de lados
+
+    public figuras2D(int nLados) {
+        this.nLados = nLados;
+    }
+
+    public int getnLados() {
+        return nLados;
+    }
+
+    public void setnLados(int nLados) {
+        this.nLados = nLados;
+    }
+
+    areaCuadrado(){
+
+    }
+    ;
 }
